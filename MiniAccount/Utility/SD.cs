1b3c0156ed9ddd5d70aa01c907ced98cba12602a
Shakib <shakibhossainmuhammad@gmail.com>
2025-06-25 18:16:02 +0600

    Add project files.

---
+++ MiniAccount/Utility/SD.cs
@@ -0,0 +1,9 @@
+﻿namespace MiniAccount.Utility
+{
+    public static class SD
+    {
+        public const string Role_Accountant = "Accountant";
+        public const string Role_Viewer = "Viewer";
+        public const string Role_Admin = "Admin";
+    }
+}
